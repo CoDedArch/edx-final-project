cc6bafdc400bf4c7c67495c7d5da65c11451e786
KELVIN KANYITI GBOLO <kelvingbolo98@gmail.com>
2023-10-17 23:35:44 +0000

    completed the automation and have tested this script

--- C_SCR/backup.sh
+++ C_SCR/backup.sh
@@ -1,13 +1,27 @@
 #! /bin/bash
 
 hst_c_svr="/c/Users/hp/LIBRARY/09_Shell_scripts/edx_final_project/HST_C_SVR"
-cd $hst_c_svr
-encr_passwd=$(find *.htpasswd)
-data=$(cat look_up.log)
+look_up="$hst_c_svr/look_up.log"
+backup_PWD="/c/Users/hp/LIBRARY/09_Shell_scripts/edx_final_project/backup_PWD"
+
+encr_passwd=$(find "$hst_c_svr" -name "*.htpasswd")
+# data=$(cat look_up.log)
 for encr_PWD in $encr_passwd
 do
-last_modified=$(stat -c "%y $encr_PWD")
-file_name=$(stat -c "%n" $encr_PWD)
+# get encrypted data
+last_modified=$(stat -c "%Y" $encr_PWD)
+file_name=$(basename $encr_PWD)
 
+log_date=$(grep "$file_name" "$look_up" | awk '{print $4}')
+if [ "$last_modified" -ne "$log_date" ]
+then
+# Backup the files
+tar -czvf "$backup_PWD/backup_$(date '+%Y%m%d').tar.gz" "$encr_PWD"
+
+#update the log to the new last modified date
+sed -i "s|$file_name.*|$file_name\tdate_modified: $last_modified|g" "$look_up"
+fi
 
 done
+
+echo "done executing"
